ded192aa7ea720b1809f76dcd7c521c5c835b657
papo9924 <papogabrriel@gmail.com>
2014-04-07 07:39:38 -0500

    user

---
+++ Kamba/src/Program.java
@@ -0,0 +1,27 @@
+import java.util.Date;
+import java.util.Random;
+
+public class Program {
+
+	public static IDashboard dashboard = new DashboardList();
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Start " + new Date());
+
+		Thread thread;
+		User user;
+		for (int i = 0; i < 15; i++) {
+			user = new User();
+			thread = new Thread(user);
+
+			thread.setName("Title " + i);
+			int priority = 1 + (int) (Math.random() * ((10 - 1) + 1));
+			thread.setPriority(priority);
+
+			thread.start();
+		}
+
+		System.out.println("End " + new Date());
+	}
+
+}
