293be79b72734c35902229a7edd2047e00204c88
papo9924 <papogabrriel@gmail.com>
2014-05-16 18:47:24 -0500

    Add

--- Kamba/src/Jpane.java
+++ Kamba/src/Jpane.java
@@ -1,18 +1,23 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.GridBagLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+
 import java.awt.Button;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
+import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 import javax.swing.JLabel;
 import javax.swing.JTextPane;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+
 import java.awt.Color;
 
 
@@ -39,6 +44,12 @@ public class Jpane extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	public Jpane(String s){
+		super(s);
+				getContentPane().setLayout(new GridBagLayout());
+		
+	}
+	
 	public Jpane() {
 		setBackground(Color.GRAY);
 		setTitle("Kamban");
@@ -50,9 +61,30 @@ public class Jpane extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		Button MARICA = new Button("ADD");
-		MARICA.setBounds(45, 263, 103, 46);
-		contentPane.add(MARICA);
+		Button ADD = new Button("ADD");
+		ADD.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Object source = e.getSource();
+				if (source instanceof Button) {
+										
+							if (JOptionPane.YES_OPTION == JOptionPane
+									.showConfirmDialog(null,
+											"Do you want to add this task?",
+											"Confirmation",
+											JOptionPane.YES_NO_OPTION,
+											JOptionPane.INFORMATION_MESSAGE)) {
+								
+							
+						
+					}
+				}
+			}
+
+		
+			}
+		);
+		ADD.setBounds(45, 263, 103, 46);
+		contentPane.add(ADD);
 		
 		JLabel lblNewLabel = new JLabel("Title");
 		lblNewLabel.setBounds(46, 11, 46, 14);
@@ -66,17 +98,17 @@ public class Jpane extends JFrame {
 		lblDescription.setBounds(46, 42, 69, 14);
 		contentPane.add(lblDescription);
 		
-		JTextPane textPane_1 = new JTextPane();
-		textPane_1.setBounds(133, 42, 129, 62);
-		contentPane.add(textPane_1);
+		JTextPane txtDescription = new JTextPane();
+		txtDescription.setBounds(133, 42, 129, 62);
+		contentPane.add(txtDescription);
 		
 		JLabel lblNewLabel_1 = new JLabel("State");
 		lblNewLabel_1.setBounds(46, 114, 46, 14);
 		contentPane.add(lblNewLabel_1);
 		
-		JTextPane textPane_2 = new JTextPane();
-		textPane_2.setBounds(133, 139, 103, 20);
-		contentPane.add(textPane_2);
+		JTextPane txtCategory = new JTextPane();
+		txtCategory.setBounds(133, 139, 103, 20);
+		contentPane.add(txtCategory);
 		
 		JLabel lblCategory = new JLabel("Category");
 		lblCategory.setBounds(46, 139, 46, 14);
@@ -102,19 +134,26 @@ public class Jpane extends JFrame {
 		lblOwner.setBounds(46, 199, 46, 14);
 		contentPane.add(lblOwner);
 		
-		JTextPane textPane_3 = new JTextPane();
-		textPane_3.setBounds(133, 193, 103, 20);
-		contentPane.add(textPane_3);
+		JTextPane txtOwner = new JTextPane();
+		txtOwner.setBounds(133, 193, 103, 20);
+		contentPane.add(txtOwner);
 		
 		JLabel lblDuedate = new JLabel("Duedate");
 		lblDuedate.setBounds(46, 224, 46, 14);
 		contentPane.add(lblDuedate);
 		
-		JTextPane textPane_4 = new JTextPane();
-		textPane_4.setBounds(133, 224, 103, 20);
-		contentPane.add(textPane_4);
+		JTextPane txtDueDate = new JTextPane();
+		txtDueDate.setBounds(133, 224, 103, 20);
+		contentPane.add(txtDueDate);
 		
 		Button button = new Button("Cancelar");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(JOptionPane.showConfirmDialog(Jpane.this, "Decea Cerrar la Ventana", "Cerrar", JOptionPane.YES_NO_OPTION)==JOptionPane.OK_OPTION){
+					dispose();
+					System.exit(1);}
+			}
+		});
 		button.setActionCommand("Cancel");
 		button.setBounds(206, 263, 93, 46);
 		contentPane.add(button);
